b9c69d67ddf41f228f0f3fc8dc756999df0340a0
lena <lll.00088@mail.ru>
2025-10-27 17:07:11 -0700

    RA-08: Create RecyclerView for categories: refactor

--- app/src/main/java/com/practicum/cookbookapp/CategoriesListAdapter.kt
+++ app/src/main/java/com/practicum/cookbookapp/CategoriesListAdapter.kt
@@ -3,25 +3,24 @@ package com.practicum.cookbookapp
 import android.graphics.drawable.Drawable
 import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.practicum.cookbookapp.databinding.ItemCategoryBinding
 
 class CategoriesListAdapter(private val dataSet: List<Category>) :
     RecyclerView.Adapter<CategoriesListAdapter.ViewHolder>() {
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val imageView: ImageView = view.findViewById(R.id.ivCardCategories)
-        val titleTextView: TextView = view.findViewById(R.id.tvCategoriesTitle)
-        val descriptionTextView: TextView = view.findViewById(R.id.tvCategoriesDescription)
+    class ViewHolder(private val binding: ItemCategoryBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        val imageView = binding.ivCardCategories
+        val titleTextView = binding.tvCategoriesTitle
+        val descriptionTextView = binding.tvCategoriesDescription
     }
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
         val inflater = LayoutInflater.from(viewGroup.context)
-        val view = inflater.inflate(R.layout.item_category, viewGroup, false)
-        return ViewHolder(view)
+        val binding = ItemCategoryBinding.inflate(inflater, viewGroup, false)
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
@@ -35,7 +34,7 @@ class CategoriesListAdapter(private val dataSet: List<Category>) :
                 null
             )
         } catch (e: Exception) {
-            Log.d("!!!", "Image not found ${category.imageUrl}")
+            Log.e("!!!", "Image not found ${category.imageUrl}, $e")
             null
         }
         viewHolder.imageView.setImageDrawable(drawable)
