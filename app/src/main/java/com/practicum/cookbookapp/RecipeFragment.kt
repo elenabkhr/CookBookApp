c0eb66f88b9a175fbf468705de8cb1ec60e3382e
lena <lll.00088@mail.ru>
2025-11-02 01:03:41 -0800

    RA-12: Implement Parcelable interface and pass Recipe object

--- app/src/main/java/com/practicum/cookbookapp/RecipeFragment.kt
+++ app/src/main/java/com/practicum/cookbookapp/RecipeFragment.kt
@@ -1,5 +1,6 @@
 package com.practicum.cookbookapp
 
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -16,11 +17,30 @@ class RecipeFragment : Fragment() {
             "Binding for FragmentRecipeBinding must not be null"
         )
 
+    var recipe: Recipe? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentRecipeBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        recipe = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            arguments?.getParcelable(ARG_RECIPE, Recipe::class.java)
+        } else {
+            arguments?.getParcelable(ARG_RECIPE)
+        }
+
+        binding.tvFragmentRecipeTitle.text = recipe?.title
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
